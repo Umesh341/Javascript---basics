dddbe368b0044881095aa3978c523f007a93b789
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-11-28 08:57:34 +0545

    Add initial HTML structure, JavaScript examples, and notes on variables, operators, and loops

---
+++ 3.Loops +Strings/index.js
@@ -0,0 +1,47 @@
+// for loop
+// let sum = 0;
+// for (let i = 1; i <= 500; i++) {
+//     sum += i;
+// }
+// console.log("Sum of numbers from 1 to 500 is: " + sum);
+
+//While loop
+// let sum = 0;
+// let i = 1;
+// while (i <= 500) {       
+//     sum += i;
+//     i++;
+// }
+// console.log("Sum of numbers from 1 to 500 is: " + sum);
+
+// Do-While loop
+// let sum = 0;
+// let i = 1;
+// do {
+//     sum += i;
+//     i++;
+// } while (i <= 500);
+// console.log("Sum of numbers from 1 to 500 is: " + sum);
+
+// For...in loop
+// let person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+// for (let key in person) {
+//     console.log(key + ": " + person[key]);
+// }
+
+// //for...of loop
+// let string = "Hello";
+// for (let char of string) {
+//     console.log(char);
+// }
+
+//printing even no from 0 to 100
+// for(let i=0; i< 101 ; i++){
+//     if(!(i%2)){
+//         console.log("even no : ", i)
+//     }
+// }
