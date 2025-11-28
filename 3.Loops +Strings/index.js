34865aad429d0a320919b579a4091f0eaa2949ab
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2025-11-28 09:26:28 +0545

    Add notes and examples for JavaScript strings and their methods

--- 3.Loops +Strings/index.js
+++ 3.Loops +Strings/index.js
@@ -45,3 +45,34 @@
 //         console.log("even no : ", i)
 //     }
 // }
+  
+
+
+// Strings
+
+let str = "Hello World";
+
+// Inbuilt methods
+console.log("Length of the string:", str.length); // 11
+console.log("Character at index 2:", str.charAt(2));  // l
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Substring (0,3):", str.substring(0, 3)); // Hel
+console.log("Index of 'World':", str.indexOf("World"));  // 6
+console.log("Replace 'Hello' with 'Hi':", str.replace("Hello", "Hi"));  // Hi World
+console.log("Concatenate with ' World':", str.concat(" World")); // Hello World World
+console.log("Trimmed string:", "   Hello   ".trim()); // "Hello"
+console.log("Split by 'e':", str.split("e")); // [ 'H', 'llo World' ]
+
+// Template Literals    
+let name = "John";
+let age = 30;
+console.log(`My name is ${name} and \n I am ${age} years old.`);
+
+let multiLineStr = `Th\nat`;
+console.log(multiLineStr.length); // 5
+
+
+let str1= "hello"
+str1[0]= "a";
+console.log(str1) //strings are immutable in js, so output will be hello
